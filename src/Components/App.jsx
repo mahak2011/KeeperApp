c8273a077e0fb7b11517c4f6a5adf0ddac59f0df
mahak2011 <mahak.raju20@gmail.com>
2021-03-04 03:41:42 +0530

    Initial Commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Notes from "../Notes";
+
+
+
+function App() {
+    return(
+    <div>
+        <Header />
+        {Notes.map(note => (
+            <Note 
+                key={note.key} 
+                title={note.title} 
+                content={note.content}
+            />
+        ))}
+        <Footer />
+    </div>);
+}
+
+export default App;
